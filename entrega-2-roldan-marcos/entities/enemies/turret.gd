399999a44ceac39d6d922413830a2bf037bb5187
roldanmarcos <guillermoroldan0@gmail.com>
2026-09-04 21:48:42 -0300

    entrega 2 completa

--- entrega-2-roldan-marcos/entities/enemies/turret.gd
+++ entrega-2-roldan-marcos/entities/enemies/turret.gd
@@ -1,10 +1,28 @@
 extends Sprite2D
 
+@export var projectile_scene:PackedScene
+
+@onready var fire_position = $FirePosition
+
+var projectile_container:Node
+
 var player
 
-func set_player(player):
+func set_values(player, projectile_container):
 	self.player = player
+	self.projectile_container = projectile_container
+	$Timer.start()
 
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	fire()
+
+func fire():
+	var projectile:Projectile = projectile_scene.instantiate()
+	projectile_container.add_child(projectile)
+	projectile.set_starting_values(fire_position.global_position, (player.global_position - fire_position.global_position).normalized())
+	projectile.delete_requested.connect(_on_projectile_delete_requested)
+
+func _on_projectile_delete_requested(projectile):
+	projectile_container.remove_child(projectile)
+	projectile.queue_free()
--- entrega-2-roldan-marcos/main.gd
+++ entrega-2-roldan-marcos/main.gd
@@ -2,4 +2,4 @@ extends Node
 
 func _ready():
 	$Player.set_projectile_container(self)
-	$Turret.set_player($Player)
+	$Turret.set_values($Player, self)
